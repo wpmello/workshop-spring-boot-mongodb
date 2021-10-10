a1df66c5cfe5b37f40c16ba8d1088810c3476d11
wpmello <wagnermello903@gmail.com>
2021-10-10 20:24:21 -0300

    FindById, service and resource classes (Post)

---
+++ src/main/java/com/example/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.example.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.workshopmongo.domain.Post;
+import com.example.workshopmongo.repositories.PostRepository;
+import com.example.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+}
