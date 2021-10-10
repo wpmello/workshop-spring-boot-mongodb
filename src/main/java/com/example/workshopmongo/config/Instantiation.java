b2ee50144caec4b97c85039a0ae524622f1636a3
wpmello <wagnermello903@gmail.com>
2021-10-10 17:09:56 -0300

    Database seeding

---
+++ src/main/java/com/example/workshopmongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.example.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.workshopmongo.domain.User;
+import com.example.workshopmongo.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		// '.deleteAll()' -> deleta todos os objetos no banco de dados
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
