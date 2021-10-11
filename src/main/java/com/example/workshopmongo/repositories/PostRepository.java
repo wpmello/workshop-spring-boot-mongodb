de679b4e59f3416ef2b90f51f6d32110752284dd
wpmello <wagnermello903@gmail.com>
2021-10-10 21:16:29 -0300

    Simple search with mongodb specifics

--- src/main/java/com/example/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/example/workshopmongo/repositories/PostRepository.java
@@ -3,12 +3,19 @@ package com.example.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.example.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
 	// ======= métodos de busca =======
+	
+	// busca simples com base no título dos posts ignorando letras maiúsculas ou minúsculas, mas agora com a 
+	// especificidade do mongoDB
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	
 	// busca simples com base no título dos posts ignorando letras maiúsculas ou minúsculas
 	List<Post> findByTitleContainingIgnoreCase(String text);
